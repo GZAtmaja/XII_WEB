a44fd747cde69a3d9332e990f092ecda82adbcbe
GZAtmaja <102244284+GZAtmaja@users.noreply.github.com>
2023-08-15 10:25:38 +0700

    Belajar upload file di github

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const downloadButton = document.getElementById
+    ("downloadButton");
+
+    downloadButton.addEventListener("click", function() {
+        const cvPath = "../assets/CV_Gazha Patra Atmaja.pdf";
+
+        const link = document.createElement("a");
+        link.href = cvPath;
+
+        link.download = "Gazha_CV.pdf";
+
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    });
+});
